f90da1093ee4d0239dbf7ae9fb0c6c0ef4b430f6
David Wendland <the.devicenator@gmail.com>
2021-05-24 16:34:08 +0200

    Add the token generator

---
+++ SniffCore.Security/ITokenGenerator.cs
@@ -0,0 +1,26 @@
+﻿//
+// Copyright (c) David Wendland. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+//
+
+namespace SniffCore.Security
+{
+    /// <summary>
+    ///     Generates a security token.
+    /// </summary>
+    public interface ITokenGenerator
+    {
+        /// <summary>
+        ///     Generates a 32 character long security token.
+        /// </summary>
+        /// <returns>The generated security token.</returns>
+        string Generate();
+
+        /// <summary>
+        ///     Generates a security token with the given length.
+        /// </summary>
+        /// <param name="length">The length of the security token.</param>
+        /// <returns>The generated security token.</returns>
+        string Generate(int length);
+    }
+}
---
+++ SniffCore.Security/TokenGenerator.cs
@@ -0,0 +1,38 @@
+﻿//
+// Copyright (c) David Wendland. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+//
+
+using System;
+using System.Security.Cryptography;
+
+namespace SniffCore.Security
+{
+    /// <summary>
+    ///     Generates a security token.
+    /// </summary>
+    public class TokenGenerator : ITokenGenerator
+    {
+        /// <summary>
+        ///     Generates a 32 character long security token.
+        /// </summary>
+        /// <returns>The generated security token.</returns>
+        public string Generate()
+        {
+            return Generate(32);
+        }
+
+        /// <summary>
+        ///     Generates a security token with the given length.
+        /// </summary>
+        /// <param name="length">The length of the security token.</param>
+        /// <returns>The generated security token.</returns>
+        public string Generate(int length)
+        {
+            var randomNumber = new byte[length];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
+    }
+}
